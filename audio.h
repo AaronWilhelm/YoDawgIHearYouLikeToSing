a74bbcb22e2a1b76d5a85fe35e54dcfe09ce9157
Aaron Wilhelm <ajw7v3@mst.edu>
2012-11-06 10:14:23 -0600

    Move note array to global.h

--- audio.h
+++ audio.h
@@ -1,8 +1,6 @@
 #ifndef AUDIO_H
 #define AUDIO_H
 
-#define SAMPLE_RATE 44100
-
 #ifdef FOR_PC
   #include <sndfile.h>
   #include <stdio.h>
@@ -29,9 +27,6 @@ struct audio_out_t
 };
 #endif
 
-static float notes [32] = {65.41, 73.42, 82.41, 87.31, 98, 110, 123.47, 130.81, 146.83, 164.81, 
-        174.61, 196, 220, 246.94, 261.63, 293.66, 329.63, 349.23, 392, 440, 493.88,
-        523.25, 587.33, 659.26, 698.46, 783.99, 880, 988, 1047, 1175, 1319, 1397};
 
 #ifdef FOR_DSP
 struct audio_in_t
--- global.h
+++ global.h
@@ -3,13 +3,21 @@
 
 #define arraysize(a) (sizeof(a)/sizeof(a[0]))
 
-#define IN_BUFF_SIZE 10
+#define IN_BUFF_SIZE 16
 #define PI 3.14159265358979323846
+#define SAMPLE_RATE 44100
 
 struct complex_t
 {
     float real;
     float imag;
 };
+
+
+static float notes [32] = {65.41, 73.42, 82.41, 87.31, 98, 110, 123.47, 130.81, 146.83, 164.81, 
+        174.61, 196, 220, 246.94, 261.63, 293.66, 329.63, 349.23, 392, 440, 493.88,
+        523.25, 587.33, 659.26, 698.46, 783.99, 880, 988, 1047, 1175, 1319, 1397};
+
+
 
 #endif /* GLOBAL_H */
